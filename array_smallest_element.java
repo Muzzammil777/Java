b866d51be703c44bea95b46b8af9062ef739a6f0
Muzzammil77 <mohammedmuzzammil.offic@gmail.com>
2025-08-08 14:01:41 +0530

    Hi

--- array_smallest_element.java
+++ array_smallest_element.java
@@ -1,6 +1,12 @@
+import java.util.*;
 public class array_smallest_element {
     public static void main(String[] args) {
-        int[] arr = {5, 2, 8, 1, 9};
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
         int smallest = arr[0];
 
         for (int i = 1; i < arr.length; i++) {
