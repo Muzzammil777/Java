ca1e221bd368a751ffee579d93d73465870a30e0
Muzzammil77 <mohammedmuzzammil.offic@gmail.com>
2025-08-08 12:04:37 +0530

    new

---
+++ reverse_the_array.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class reverse_the_array {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int a[] = new int[n];
+
+        for(int i=0;i<n;i++){
+            a[i] = sc.nextInt();
+        }
+
+        for(int i=n;i>1;i--){
+            System.out.print(a[i] + " ");
+        }
+
+
+
+    }  
+}
